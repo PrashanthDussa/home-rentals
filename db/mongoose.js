8d04511d66bdac34c36e75055cf945953b3d7e3c
Prashanth Dussa <prashanth.dussa05@gmail.com>
2020-11-11 13:20:30 +0530

    modified

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(
+  'mongodb+srv://prashanthdussa:<dussa2000>@home-rentals.ypnzy.mongodb.net/<users>?retryWrites=true&w=majority'
+);
 
 module.exports = { mongoose };
